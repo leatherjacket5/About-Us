f3e797804ce6d06a98bd53986520f92bfac9998a
Lili Boxer <ninaliboxer@gmail.com>
2019-04-23 13:01:17 -0700

    submit button callback function

---
+++ index.js
@@ -0,0 +1,11 @@
+const submitButton = document.getElementById('submit-button');
+const roosterInput = document.getElementById('rooster-ask');
+const leatherInput = document.getElementById('leather-ask');
+const phoenixInput = document.getElementById('phoenix-ask');
+const rockyInput = document.getElementById('rocky-ask');
+const trickInput = document.getElementById('trick-ask');
+const  quizResult = document.getElementById('quiz-result');
+
+submitButton.addEventListener('click', () => {
+
+}); 
