7144041b019e7718c64f4bab2beecb0c0c89b1b6
Lili Boxer <ninaliboxer@gmail.com>
2019-04-23 13:22:29 -0700

    submit tested submt functions

--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ const trickInput = document.getElementById('trick-ask');
 const  quizResult = document.getElementById('quiz-result');
 
 submitButton.addEventListener('click', () => {
+    const roosterAnswer = roosterInput.value;
+    const leatherAnswer = leatherInput.value;
+    const phoenixAnswer = phoenixInput.value;
+    const rockyAnswer = rockyInput.value;
+    const trickAnswer = trickInput.value;
+    console.log(roosterAnswer);
+    
+});
 
-}); 
